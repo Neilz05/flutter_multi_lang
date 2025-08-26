028e91a0c536072b8c04f434bf5c351e43c0296d
Neil Dong <neildong@Mac-Mini.local>
2025-08-26 17:21:43 +0800

    [Feature]: Flutter Multi Language
    Description
    - Initial implementation of localized messages in a flutter application

---
+++ lib/l10n/app_localizations_ja.dart
@@ -0,0 +1,18 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Japanese (`ja`).
+class AppLocalizationsJa extends AppLocalizations {
+  AppLocalizationsJa([String locale = 'ja']) : super(locale);
+
+  @override
+  String get helloWorld => 'Hello World!';
+
+  @override
+  String hello(String userName) {
+    return 'こんにちは $userName';
+  }
+}
